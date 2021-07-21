476578b89be8016a7eeefc4bf4221ddaa07475be
rightlit <rightlit@gmail.com>
2021-07-21 02:38:22 +0000

    src update

--- models/kogpt2_transformers/load_model.py
+++ models/kogpt2_transformers/load_model.py
@@ -4,7 +4,7 @@ def get_kogpt2_model(model_path=None):
     if not model_path:
         model_path = 'taeminlee/kogpt2'
         #model_path = 'kogpt2'
-    model = GPT2LMHeadModel.from_pretrained(model_path, cache_dir='./')
+    model = GPT2LMHeadModel.from_pretrained(model_path, cache_dir='./cache')
     #print(model)
     return model
 
@@ -12,6 +12,6 @@ def get_kogpt2_tokenizer(model_path=None):
     if not model_path:
         model_path = 'taeminlee/kogpt2'
         #model_path = 'kogpt2'
-    tokenizer = PreTrainedTokenizerFast.from_pretrained(model_path, cache_dir='./')
+    tokenizer = PreTrainedTokenizerFast.from_pretrained(model_path, cache_dir='./cache')
     #print(tokenizer)
     return tokenizer

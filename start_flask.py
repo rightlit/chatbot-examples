ea8cc530de99f1238af114c94900a2113463a625
jeongwook lee <rightlit@gmail.com>
2022-02-03 16:35:55 +0900

    Update start_flask.py

--- start_flask.py
+++ start_flask.py
@@ -11,7 +11,7 @@ def board():
 # URL 에 매개변수를 받아 진행하는 방식입니다.
 @app.route('/board/<article_idx>')
 def board_view(article_idx):
-    return article_idx
+    return '게시판 번호: ', article_idx
 
 # 위에 있는것이 Endpoint 역활을 해줍니다.
 @app.route('/boards',defaults={'page':'index'})

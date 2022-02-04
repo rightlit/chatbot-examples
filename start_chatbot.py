8386003b7a4f43e61f78fd1e5314b1f4e797ae46
jeongwook lee <rightlit@gmail.com>
2022-02-04 11:23:56 +0900

    Update start_chatbot.py

--- start_chatbot.py
+++ start_chatbot.py
@@ -147,7 +147,7 @@ def query(query_str):
     for i in score.argsort()[::-1]:
         if score[i] > 0:
             print('{} / score : {}'.format(rawdata_q[i], score[i]))
-        ret_str = ret_str + '{} / score : {}'.format(rawdata_q[i], score[i])
+        ret_str = ret_str + '\n' + '{} / score : {}'.format(rawdata_q[i], score[i])
 
         cnt = cnt + 1
         if(cnt > 10):
